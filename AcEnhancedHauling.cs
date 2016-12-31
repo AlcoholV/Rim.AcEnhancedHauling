776945164cc19f7554ce5a27dd30272143f5e21a
AlcholV <robert@mindnect.com>
2017-01-01 00:19:27 +0900

    A16 1.0

---
+++ AcEnhancedHauling.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace AlcoholV
+{
+    public static class AcEnhancedHauling
+    {
+        public const float NoneHaulDistance = 16f;
+
+        public static Job SmartBill(Pawn p, Job job)
+        {
+            if (p.story.WorkTagIsDisabled(WorkTags.Hauling))
+            {
+                return job;
+            }
+            var carryCapacity = p.GetStatValue(StatDefOf.CarryingCapacity);
+            for (var i = 0; i < job.targetQueueB.Count; i++)
+            {
+                var thing = job.targetQueueB[i].Thing;
+                var storagePriority = HaulAIUtility.StoragePriorityAtFor(thing.Position, thing);
+                var itemCount = job.countQueue[i];
+                IntVec3 foundPos;
+                if (StoreUtility.TryFindBestBetterStoreCellFor(thing, p, thing.Map,storagePriority, p.Faction, out foundPos) && carryCapacity >= itemCount)
+                {
+                    var thingPos = thing.Position;
+                    var targetPos = job.targetA.Thing.Position;
+                    if ((targetPos - thingPos).LengthHorizontalSquared > (targetPos - foundPos).LengthHorizontalSquared)
+                    {
+                        return HaulAIUtility.HaulMaxNumToCellJob(p, thing, foundPos, false);
+                    }
+                }
+            }
+            return job;
+        }
+
+        public static Job SmartBuild(Pawn p, Job job)
+        {
+            if (p.story.WorkTagIsDisabled(WorkTags.Hauling))
+            {
+                return job;
+            }
+            var carryCapacity = p.GetStatValue(StatDefOf.CarryingCapacity);
+            var itemCount = job.count;
+            var thing = job.targetA.Thing;
+            var storagePriority = HaulAIUtility.StoragePriorityAtFor(thing.Position, thing);
+
+
+            var foundPos = new IntVec3();
+            if (StoreUtility.TryFindBestBetterStoreCellFor(thing, p, thing.Map, storagePriority, p.Faction, out foundPos) && carryCapacity >= itemCount)
+            {
+
+                var thingPos = thing.Position;
+                var targetPos = job.targetB.Thing.Position;
+                if ((targetPos - thingPos).LengthHorizontalSquared > (targetPos - foundPos).LengthHorizontalSquared)
+                {
+                    return HaulAIUtility.HaulMaxNumToCellJob(p, thing, foundPos, false);
+                }
+                return HaulAIUtility.HaulMaxNumToCellJob(p, thing, foundPos, false);
+            }
+
+            return job;
+        }
+    }
+}
---
+++ WorkGiver.cs
@@ -0,0 +1,58 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace AlcoholV
+{
+    public class WorkGiver_DoBill : RimWorld.WorkGiver_DoBill
+    {
+        public override Job JobOnThing(Pawn pawn, Thing thing)
+        {
+            var job = base.JobOnThing(pawn, thing);
+            if (job == null)
+            {
+                return null;
+            }
+            if ((job.def != JobDefOf.DoBill) || job.targetQueueB[0].Thing.def.isUnfinishedThing)
+            {
+                return job;
+            }
+            return AcEnhancedHauling.SmartBill(pawn, job);
+        }
+    }
+
+    public class WorkGiver_ConstructDeliverResourcesToFrames : RimWorld.WorkGiver_ConstructDeliverResourcesToFrames
+    {
+        public override Job JobOnThing(Pawn pawn, Thing t)
+        {
+            var job = base.JobOnThing(pawn, t);
+            if (job == null)
+            {
+                return null;
+            }
+            if (job.def != JobDefOf.HaulToContainer)
+            {
+                return job;
+            }
+            return AcEnhancedHauling.SmartBuild(pawn, job);
+        }
+    }
+
+    internal class WorkGiver_ConstructDeliverResourcesToBlueprints : RimWorld.WorkGiver_ConstructDeliverResourcesToBlueprints
+    {
+        public override Job JobOnThing(Pawn pawn, Thing t)
+        {
+            var job = base.JobOnThing(pawn, t);
+            if (job == null)
+            {
+                return null;
+            }
+            var blueprintInstall = (Blueprint_Install) t;
+            if ((job.def != JobDefOf.HaulToContainer) || (blueprintInstall != null))
+            {
+                return job;
+            }
+            return AcEnhancedHauling.SmartBuild(pawn, job);
+        }
+    }
+}
